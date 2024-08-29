12c37a9ddbef8cb65f0f54767720ad080415e6b7
Darcy Zhu <darcy.zhu>
2024-08-29 11:11:04 +0800

    feat: del插件

--- babel.config.js
+++ babel.config.js
@@ -1,18 +1,6 @@
 const isDev = process.env.NEXT_PUBLIC_NODE_ENV === 'development';
-
-const basePlugins = [
-  [
-    'babel-plugin-injectcomponentcode',
-    {
-      importComponentFilePath: '@/components/PageWrapper',
-      importComponentName: 'PageWrapper',
-      isImportDefault: true,
-    },
-  ],
-];
 const babelPlugins = isDev
   ? [
-      ...basePlugins,
       [
         'babel-plugin-jsxfileattribute',
         {
